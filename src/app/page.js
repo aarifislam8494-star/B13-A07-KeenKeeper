f2c483797c5e000e9c5b6bc2a9862cadcb407806
aarifislam8494-star <aarifislam8494@gmail.com>
2026-04-14 23:00:02 +0600

    add stats section & display friend cards on homepage

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,14 @@
 import Banner from "@/components/homepage/Banner";
+import Friends from "@/components/homepage/Friends";
+import Stats from "@/components/homepage/Stats";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <Banner />
+      <Stats />
+      <Friends />
     </div>
   );
 }
